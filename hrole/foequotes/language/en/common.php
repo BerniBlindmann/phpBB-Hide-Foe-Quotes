5e0d87778a5b6fb8830989ae50abb2e8131edec2
BerniBlindmann <122235371+BerniBlindmann@users.noreply.github.com>
2026-09-11 01:01:54 +0200

    Add files via upload

--- hrole/foequotes/language/en/common.php
+++ hrole/foequotes/language/en/common.php
@@ -1,1 +1,23 @@
+<?php
+/**
+ *
+ * Hide Foe Quotes. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2026
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
 
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'FOE_QUOTE_HIDDEN' => 'A quote from a member you are ignoring was hidden.',
+));
